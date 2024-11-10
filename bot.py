9c48cc715042329a94572849c1f4378f864d8393
jackkurd <mahamadpshtewan0@gmail.com>
2024-11-10 03:24:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from Youtube.config import Config
 
 # Create a Pyrogram client
 app = Client(
-    "my_bot",
+    "@YouTube_Jack_Download_robot",
     api_id=Config.20598977, 
     api_hash=Config.6fdd752f56a5715ea0819e970fb12e9d ,
     bot_token=Config.7342515934:AAEkGWHPMUsGAfXwce_WpJDJHAEBIusJFFw,
